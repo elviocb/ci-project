f2785cded49bda11b35df45b1afd8d6db15dd05f
Elvio Cavalcante <elvio.cavalcante@toptal.com>
2022-02-07 16:21:07 -0300

    add new features

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -11676,13 +11676,26 @@ const core = __nccwpck_require__(2186)
 const github = __nccwpck_require__(5438)
 
 try {
+  // This will only work when ACTIONS_STEP_DEBUG secret is true
+  core.debug('Some debug message')
+  core.warning('Some warning message')
+  core.error('Some error message')
+
   const name = core.getInput('who-to-greet')
+  // this will log the name as ***
+  core.setSecret(name)
   console.log(`Hello ${name}`)
 
   const time = new Date()
   core.setOutput('time', time.toTimeString())
 
+  // This will create a group
+  core.startGroup('Some log')
   console.log(JSON.stringify(github, null, '\t'))
+  core.endGroup()
+
+  // This will create an ENV variable that the workflow can use
+  core.exportVariable('HELLO', 'hello')
 } catch (error) {
   core.setFailed(error.message)
 }
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,13 +2,26 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 try {
+  // This will only work when ACTIONS_STEP_DEBUG secret is true
+  core.debug('Some debug message')
+  core.warning('Some warning message')
+  core.error('Some error message')
+
   const name = core.getInput('who-to-greet')
+  // this will log the name as ***
+  core.setSecret(name)
   console.log(`Hello ${name}`)
 
   const time = new Date()
   core.setOutput('time', time.toTimeString())
 
+  // This will create a group
+  core.startGroup('Some log')
   console.log(JSON.stringify(github, null, '\t'))
+  core.endGroup()
+
+  // This will create an ENV variable that the workflow can use
+  core.exportVariable('HELLO', 'hello')
 } catch (error) {
   core.setFailed(error.message)
 }
