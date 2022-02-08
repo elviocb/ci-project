704b099db612e4d7bc02bff963ae2cea9248edab
Elvio Cavalcante <elvio.cavalcante@toptal.com>
2022-02-07 21:54:54 -0300

    add new logs

--- .github/actions/pr-checker/index.js
+++ .github/actions/pr-checker/index.js
@@ -4,9 +4,15 @@ const github = require('@actions/github')
 const TICKET_REGEX = /\[\w+-\d+\]/
 const BYPASS_LABEL = 'no-ticket'
 const SUCCESS_MESSAGE = 'Thank you for connection the PR with a ticket.'
+const BYPASS_MESSAGE =
+  'The label to bypass this check was found, no checks will be performed.'
 
-const getErrorMessage = pullRequestType =>
-  `Please connect the PR's ${pullRequestType} to a ticket or add the "${BYPASS_LABEL}" label to bypass this check.`
+const setErrorMessage = pullRequestType =>
+  core.setFailed(
+    `Please connect the PR's ${pullRequestType} to a ticket or add the "${BYPASS_LABEL}" label to bypass this check.`
+  )
+const setSuccessMessage = () => core.setOutput('pull-request', SUCCESS_MESSAGE)
+const setBypassMessage = () => core.setOutput('pull-request', BYPASS_MESSAGE)
 
 async function run() {
   try {
@@ -15,20 +21,29 @@ async function run() {
     const { body, title, labels } = github.context.payload.pull_request
     console.log(JSON.stringify(labels, null, '\t'))
     console.log(JSON.stringify(github, null, '\t'))
+    const shouldBypass = labels.map(label => label.name).includes(BYPASS_LABEL)
     const titleMatches = title.match(TICKET_REGEX)
     const bodyMatches = body.match(TICKET_REGEX)
 
+    core.info('Output to the actions build log')
+    core.notice('This is a message that will also emit an annotation')
+
+    if (shouldBypass) {
+      setBypassMessage()
+      return
+    }
+
     if (!titleMatches) {
-      core.setFailed(getErrorMessage('title'))
+      setErrorMessage('title')
       return
     }
 
     if (!bodyMatches) {
-      core.setFailed(getErrorMessage('body'))
+      setErrorMessage('body')
       return
     }
 
-    core.setOutput('pull-request', SUCCESS_MESSAGE)
+    setSuccessMessage()
   } catch (error) {
     core.error(error)
     core.setFailed(error.message)
